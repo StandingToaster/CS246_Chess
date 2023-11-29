06cda285251dc8eb448e7644b2bdc3ff52ccefb6
Harsimran <harsimrankalsi123@gmail.com>
2023-11-29 16:33:09 -0500

    level2

--- level2.cc
+++ level2.cc
@@ -12,8 +12,26 @@ Move level2::generateMove() {
 
     //continues running until a move that results in a kill or check isfound
     for (Move m : validMoves) {
-        //implement logic here
-        return m;
+        if (m.hasEnemyPresence()) { // checks for kill
+            return m;
+        }
+        
+        Board temp = *b;
+        if(temp.activateMove(m.getStart(), m.getDest())) {
+            Colour enemyColour;
+            if (this->getColour() == Colour::Black) {
+                enemyColour = Colour::White;
+            } else {
+                enemyColour = Colour::Black;
+            }
+
+
+            if (temp.checked(enemyColour)) {
+                return m;
+            }
+        }
     }
-    
+
+    // if loop exited, just return first valid move from array
+    return validMoves[0];
 }
