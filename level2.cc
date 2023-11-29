a7e5852c6e5cdf533dabab6bac35c404e42f3aa7
Hirav T <hirav.a.s.thakur@gmail.com>
2023-11-29 16:14:11 -0500

    Amended checkForKill in level2: public Computer.

--- level2.cc
+++ level2.cc
@@ -2,21 +2,7 @@
 
 using namespace std;
 
-vector<Move> level2::checkForKill() {
-    Board* p = this->getPointer();
-    Colour cur = this->getColour();
-    //Vector that will return potential moves that result in kills.
-    vector<Move> temp = {};
-    vector<Move> LegalMoves = p->getLegalMoves(cur);
-    //Loop through all possible legal moves, determine if any result in kills, if so, add to new vector "temp".
-    for (long unsigned int i = 0; i < LegalMoves.size(); ++i) {
-        if (LegalMoves[i].hasEnemyPresence()) {
-            temp.emplace_back(LegalMoves[i]);
-        }
-        else {
-            continue;
-        }
-    }
-    return temp;
+bool level2::checkForKill(Move input) {
+   return input.hasEnemyPresence();
 }
 
