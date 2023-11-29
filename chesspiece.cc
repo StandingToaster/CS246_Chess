e0f4f0392904f91a54bb9f3f6c9e5f0446b5b651
Jinil Panawala <jinilpanawala2004@gmail.com>
2023-11-28 22:59:28 -0500

    rook legal moves complete

--- chesspiece.cc
+++ chesspiece.cc
@@ -463,7 +463,92 @@ bool Rook::canAttack(Cell & start, Cell & destination, Board & b) {
 
 }
 void Rook::determineLegalMoves(Cell & start, Board & b) {
+    int sx = start.getX();
+    int sy = start.getY();
 
+    if (start.getChessPiece() == nullptr) { // start piece non-existant
+        return;
+    }
+    if (start.getChessPiece()->getPiece() != Piece::Rook) { // start piece is not rook
+        return;
+    }
+
+    // moving vertically
+    // moving up
+    int y = sy - 1;
+
+    while (y >= 0) {
+        if (!b.cellEmpty(sx, y) && b.getCell(sx, y).getChessPiece()->getColour() != start.getChessPiece()->getColour()) {
+            b.addBlackOrWhiteLegalMove(Move{start, b.getCell(sx,y)});
+            break;
+        }
+
+        if (!b.cellEmpty(sx, y) && b.getCell(sx, y).getChessPiece()->getColour() == start.getChessPiece()->getColour()) {
+            break;
+        }
+
+        if (b.cellEmpty(sx, y)) {
+            b.addBlackOrWhiteLegalMove(Move{start, b.getCell(sx,y)});
+        }
+        y--;
+    }
+
+    // moving down
+    y = sy + 1;
+    while (y < b.getBoardSize()) {
+        if (!b.cellEmpty(sx, y) && b.getCell(sx, y).getChessPiece()->getColour() != start.getChessPiece()->getColour()) {
+            b.addBlackOrWhiteLegalMove(Move{start, b.getCell(sx,y)});
+            break;
+        }
+
+        if (!b.cellEmpty(sx, y) && b.getCell(sx, y).getChessPiece()->getColour() == start.getChessPiece()->getColour()) {
+            break;
+        }
+
+        if (b.cellEmpty(sx, y)) {
+            b.addBlackOrWhiteLegalMove(Move{start, b.getCell(sx,y)});
+        }
+        y++;
+    }
+
+
+    // moving horizontally
+    // moving left
+    int x = sx - 1;
+
+    while (x >= 0) {
+        if (!b.cellEmpty(x, sy) && b.getCell(x, sy).getChessPiece()->getColour() != start.getChessPiece()->getColour()) {
+            b.addBlackOrWhiteLegalMove(Move{start, b.getCell(x,sy)});
+            break;
+        }
+
+        if (!b.cellEmpty(x, sy) && b.getCell(x, sy).getChessPiece()->getColour() == start.getChessPiece()->getColour()) {
+            break;
+        }
+
+        if (b.cellEmpty(x, sy)) {
+            b.addBlackOrWhiteLegalMove(Move{start, b.getCell(x,sy)});
+        }
+        x--;
+    }
+
+    // moving right
+    x = sx + 1;
+    while (x < b.getBoardSize()) {
+        if (!b.cellEmpty(x, sy) && b.getCell(x, sy).getChessPiece()->getColour() != start.getChessPiece()->getColour()) {
+            b.addBlackOrWhiteLegalMove(Move{start, b.getCell(x,sy)});
+            break;
+        }
+
+        if (!b.cellEmpty(x, sy) && b.getCell(x, sy).getChessPiece()->getColour() == start.getChessPiece()->getColour()) {
+            break;
+        }
+
+        if (b.cellEmpty(x, sy)) {
+            b.addBlackOrWhiteLegalMove(Move{start, b.getCell(x,sy)});
+        }
+        x++;
+    }
 }
 
 
