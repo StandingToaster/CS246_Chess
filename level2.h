ba6bff558ddbc024bebed2b5bd468a09d2333db7
Harsimran <harsimrankalsi123@gmail.com>
2023-11-29 17:35:55 -0500

    level3

--- level2.h
+++ level2.h
@@ -7,5 +7,7 @@ class level2 : public Computer {
     //Determines whether the given move will result in a kill or not.
      bool checkForKill(Move input);
     public:
+    //level 2 will generate will output a capturing/check move if possible and
+    //a random move otherwise
     Move generateMove() override;
 };
--- level3.cc
+++ level3.cc
@@ -1,3 +1,39 @@
 #include "level3.h"
 
 using namespace std;
+
+bool GenearteCaptureOrCheckMove(level3& comp, Move& move) {
+    Board* b = comp.getPointer();
+    vector<Move> validMoves = b->getLegalMoves(comp.getColour());
+
+    //continues running until a move that results in a kill or check isfound
+    for (Move m : validMoves) {
+        if (m.hasEnemyPresence()) { // checks for kill
+            move = m;
+            return true;
+        }
+        
+        Board temp = *b;
+        if(temp.activateMove(m.getStart(), m.getDest())) {
+            Colour enemyColour;
+            if (comp.getColour() == Colour::Black) {
+                enemyColour = Colour::White;
+            } else {
+                enemyColour = Colour::Black;
+            }
+
+
+            if (temp.checked(enemyColour)) {
+                move = m;
+                return true;
+            }
+        }
+    }
+
+    // if loop exited, no capture/check move was found and return false
+    return false;
+}
+
+Move level3::generateMove() {
+    
+}
