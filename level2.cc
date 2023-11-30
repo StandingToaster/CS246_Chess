7e45eb0f3f4c69966ee7155815a29b00829e8e66
Jinil Panawala <jinilpanawala2004@gmail.com>
2023-11-30 14:23:12 -0500

    small change in level2.cc

--- level2.cc
+++ level2.cc
@@ -11,13 +11,19 @@ Move level2::generateMove() {
 
     //continues running until a move that results in a kill or check isfound
     for (Move m : validMoves) {
+
         if (m.hasEnemyPresence()) { // checks for kill
             return m;
         }
         
         Board temp = *b;
         // SHOULD BE temp.getCell(x,y) !!!!!!!!!!!!!!!!!!!!!!!!
-        if(temp.activateMove(m.getStart(), m.getDest())) {
+        int sx = m.getStart().getX();
+        int sy = m.getStart().getY();
+        int dx = m.getDest().getX();
+        int dy = m.getDest().getY();
+
+        if (temp.activateMove(temp.getCell(sx, sy), temp.getCell(dx, dy))) {
             Colour enemyColour;
             if (this->getColour() == Colour::Black) {
                 enemyColour = Colour::White;
@@ -25,14 +31,14 @@ Move level2::generateMove() {
                 enemyColour = Colour::Black;
             }
 
-
             if (temp.checked(enemyColour)) {
                 return m;
             }
         }
     }
+
     //Now if no enemyPresence is detected or a check is possible, it will pick a RANDOM valid move.
-      int count = rand() % validMoves.size();
+    int count = rand() % validMoves.size();
     return validMoves[count];
 }
 
