4ce2cb8f2c99197c4d8635b49e504a5e0d7ae45e
Hirav T <hirav.a.s.thakur@gmail.com>
2023-11-29 23:35:14 -0500

    Edited leve2.h, removed private method.

--- level2.h
+++ level2.h
@@ -6,8 +6,6 @@ using namespace std;
 
 
 class level2 : public Computer {
-    //Determines whether the given move will result in a kill or not.
-     bool checkForKill(Move input);
     public:
     //level 2 will generate will output a capturing/check move if possible and
     //a random move otherwise
