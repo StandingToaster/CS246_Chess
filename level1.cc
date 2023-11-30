c0597b0c21e288f00f081d87499a16d1564302db
Hirav T <hirav.a.s.thakur@gmail.com>
2023-11-30 15:11:57 -0500

    Attempted fix of RNG for level1.cc

--- level1.cc
+++ level1.cc
@@ -15,6 +15,8 @@ Move level1::generateMove() {
         vector<Move> temp = p->getLegalMoves(this->getColour());
         //Retrieves vector size.
         int N = temp.size();
+        //Specifics for how a C++ generator works, ignore!
+        srand((unsigned) time(NULL));
         //Generates random number between 0 and N-1 (Upper bound of vector).
         int random = rand() % N;
         //Returns the move chosen by computer.
@@ -25,6 +27,7 @@ Move level1::generateMove() {
         //Repeat but only if the computer is playing white side instead.
        vector<Move> temp = p->getLegalMoves(this->getColour());
         int N = temp.size();
+        srand((unsigned) time(NULL));
         int random = rand() % N;
         return temp[random]; 
     }
