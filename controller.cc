8aa6be89c3ad5fa0f26cc01dbc8c2b9c71948d41
Hirav T <hirav.a.s.thakur@gmail.com>
2023-12-03 20:16:01 -0500

    amending human vs human

--- controller.cc
+++ controller.cc
@@ -163,10 +163,6 @@ void Controller::playGame(istream &in, ostream &out) {
             // Create new game, follow specifications
             currentBoard->setDefaultBoard();
             out << *currentBoard << endl;
-
-            if (p1 == "human" && p2 == "human") {
-                humanVsHuman(in, out);
-            }
             
         }
         else if (temp == "move") {
