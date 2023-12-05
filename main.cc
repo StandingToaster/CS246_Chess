3799742c3d2eb2acb35c4c0e18244282f8824515
Hirav T <hirav.a.s.thakur@gmail.com>
2023-12-04 22:28:52 -0500

    sm idk

--- main.cc
+++ main.cc
@@ -51,6 +51,11 @@ int main() {
     Controller * c = new Controller{&b};
     c->playGame(cin, cout);
 
+    cout << "Final Score:" << endl;
+    cout << "White: " <<  c->score1 << endl;
+    cout << "Black: " << c->score2 << endl;
+
+    return 0;
 
 
 
